3bad8540e091d375ea983cace89fbf68231ff538
Brandon Clapp <brandonclapp@outlook.com>
2019-06-08 08:43:13 -0500

    Map init

---
+++ maps/TownColor2@128x128.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="TownColor2@128x128" tilewidth="128" tileheight="128" tilecount="256" columns="16">
+ <image source="assets/128/TownColor2@128x128.png" width="2048" height="2048"/>
+ <terraintypes>
+  <terrain name="Road" tile="126"/>
+ </terraintypes>
+ <tile id="109" terrain=",,,0"/>
+ <tile id="110" terrain=",,0,0"/>
+ <tile id="111" terrain=",,0,"/>
+ <tile id="125" terrain=",0,,0"/>
+ <tile id="126" terrain="0,0,0,0"/>
+ <tile id="127" terrain="0,,0,"/>
+ <tile id="141" terrain=",0,,"/>
+ <tile id="142" terrain="0,0,,"/>
+ <tile id="143" terrain="0,,,"/>
+ <tile id="158" terrain=",0,0,0"/>
+ <tile id="159" terrain="0,,0,0"/>
+ <tile id="174" terrain="0,0,,0"/>
+ <tile id="175" terrain="0,0,0,"/>
+ <tile id="190" terrain="0,0,0,0"/>
+ <tile id="191" terrain="0,0,0,0"/>
+</tileset>
