a4344e5fe2c14a4960eae983341e9f773ec1e3b2
Brandon Clapp <brandonclapp@outlook.com>
2019-06-08 14:51:42 -0500

    Map changes

---
+++ maps/Shadows@64x64.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Shadows@64x64" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <tileoffset x="-2" y="0"/>
+ <image source="assets/64/Shadows@64x64.png" width="256" height="256"/>
+</tileset>
---
+++ maps/TownColor2@64x64.tsx
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="TownColor2@64x64" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="assets/64/TownColor2@64x64.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="Stone Road" tile="126"/>
+  <terrain name="Rugged Road" tile="169"/>
+  <terrain name="Water" tile="227"/>
+ </terraintypes>
+ <tile id="92" probability="0.01"/>
+ <tile id="95" probability="0.1"/>
+ <tile id="109" terrain=",,,0"/>
+ <tile id="110" terrain=",,0,0"/>
+ <tile id="111" terrain=",,0,"/>
+ <tile id="125" terrain=",0,,0"/>
+ <tile id="126" terrain="0,0,0,0"/>
+ <tile id="127" terrain="0,,0,"/>
+ <tile id="141" terrain=",0,,"/>
+ <tile id="142" terrain="0,0,,"/>
+ <tile id="143" terrain="0,,,"/>
+ <tile id="152" terrain=",,,1"/>
+ <tile id="153" terrain=",,1,1"/>
+ <tile id="154" terrain=",,1,"/>
+ <tile id="155" terrain=",1,1,1"/>
+ <tile id="156" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="157" terrain="1,,1,1"/>
+ <tile id="158" terrain=",0,0,0"/>
+ <tile id="159" terrain="0,,0,0"/>
+ <tile id="168" terrain=",1,,1"/>
+ <tile id="169" terrain="1,1,1,1" probability="0.8"/>
+ <tile id="170" terrain="1,,1,"/>
+ <tile id="171" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="172" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="173" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="174" terrain="0,0,,0"/>
+ <tile id="175" terrain="0,0,0,"/>
+ <tile id="184" terrain=",1,,"/>
+ <tile id="185" terrain="1,1,,"/>
+ <tile id="186" terrain="1,,,"/>
+ <tile id="187" terrain="1,1,,1"/>
+ <tile id="188" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="189" terrain="1,1,1,"/>
+ <tile id="190" terrain="0,0,0,0"/>
+ <tile id="191" terrain="0,0,0,0"/>
+ <tile id="209" terrain="2,2,2,2"/>
+ <tile id="210" terrain=",,,2"/>
+ <tile id="211" terrain=",,2,2"/>
+ <tile id="212" terrain=",,2,"/>
+ <tile id="224" terrain="2,2,2,"/>
+ <tile id="225" terrain="2,2,,2"/>
+ <tile id="226" terrain=",2,,2"/>
+ <tile id="228" terrain="2,,2,"/>
+ <tile id="240" terrain="2,,2,2"/>
+ <tile id="241" terrain=",2,2,2"/>
+ <tile id="242" terrain=",2,,"/>
+ <tile id="243" terrain="2,2,,"/>
+ <tile id="244" terrain="2,,,"/>
+</tileset>
